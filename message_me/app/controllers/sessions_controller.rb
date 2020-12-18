a8f4efdd52f06fe53d4039f4125541c670bad1b6
Luke Dziewanowski <lukasz.dziewanowski@gmail.com>
2020-12-18 11:39:11 +0100

    Basic login and session creation

--- message_me/app/controllers/sessions_controller.rb
+++ message_me/app/controllers/sessions_controller.rb
@@ -1,7 +1,32 @@
 class SessionsController < ApplicationController
 
-  # GET /sessions/new
+  # GET /login
   def new
   end
 
+  # POST /login
+  def create
+    user = User.find_by(username: session_params[:username])
+    if user && user.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      flash.notice = "Logged in successfully"
+      redirect_to root_path
+    else
+      flash.now.alert = "There was something wrong with your login detail"
+      render 'new'
+    end
+  end
+
+  # DELETE /login
+  def destroy
+    session[:user_id] = nil
+    flash.notice = "Logged out"
+    redirect_to root_path
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:username, :password)
+  end
 end
