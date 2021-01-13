e79ef7798fe7df71c5c1b016a231b3bd39b59f50
Luke Dziewanowski <lukasz.dziewanowski@gmail.com>
2021-01-13 22:34:18 +0100

    Rendering message

--- message_me/app/controllers/messages_controller.rb
+++ message_me/app/controllers/messages_controller.rb
@@ -7,13 +7,17 @@ class MessagesController < ApplicationController
     if !@message.save
       flash.alert = "Message couldn't be added ..."
     end
-    ActionCable.server.broadcast "chatroom_channel", foo: @message.body
+    ActionCable.server.broadcast "chatroom_channel", mod_message: message_render(@message)
   end
 
   private
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: {message: message})
   end
 
 end
