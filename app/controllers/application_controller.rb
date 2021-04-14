d5e6807dcbf3b024f2a4ac74434ad5758326f475
Luke Dziewanowski <lukasz.dziewanowski@gmail.com>
2021-04-14 16:10:16 +0200

    Custom layout for devise and partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
   before_action :authenticate_user!
+
+  private
+
+  # https://github.com/heartcombo/devise/wiki/How-To:-Create-custom-layouts
+  def layout_by_resource
+    if devise_controller?
+      'devise'
+    else
+      'application'
+    end
+  end
 end
