c0ba9e4e16a76e78bcda01d303f4dae4587879b8
Luke Dziewanowski <lukasz.dziewanowski@gmail.com>
2021-04-15 14:33:17 +0200

    Stocks controller and route

--- stock_tracker/config/routes.rb
+++ stock_tracker/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_porfolio', to: 'users#my_portfolio'
 
+  get 'search_stock', to: 'stocks#search'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
