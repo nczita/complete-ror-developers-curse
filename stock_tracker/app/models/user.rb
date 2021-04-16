2d38a24f5181e8ae5ba1406bd7e331e766bf6791
Luke Dziewanowski <lukasz.dziewanowski@gmail.com>
2021-04-16 15:39:12 +0200

    Some restrictions about adding stock by user

--- stock_tracker/app/models/user.rb
+++ stock_tracker/app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
+  MAX_OF_STOCKS = 3
+
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
-  
+
+  validates_length_of :stocks, maximum: MAX_OF_STOCKS
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+  def can_track_stock?(ticker_symbol)
+    stocks.count < MAX_OF_STOCKS && !stocks.find_by(ticker: ticker_symbol)
+  end
+end 
