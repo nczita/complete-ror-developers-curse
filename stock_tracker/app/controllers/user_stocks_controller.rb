0c8869bca7ca227ce73f5c3ced2e99e836aa4663
Luke Dziewanowski <lukasz.dziewanowski@gmail.com>
2021-04-16 15:23:18 +0200

    Adding stock for user

--- stock_tracker/app/controllers/user_stocks_controller.rb
+++ stock_tracker/app/controllers/user_stocks_controller.rb
@@ -1,2 +1,9 @@
 class UserStocksController < ApplicationController
+  def create
+    stock = Stock.find_by(ticker: params[:ticker]) || Stock.new_lookup(params[:ticker])
+    stock.save
+    @user_stock = UserStock.create(user: current_user, stock: stock)
+    flash[:notice] = "Stock #{stock.name} was added to your portfolio"
+    redirect_to my_portfolio_path
+  end
 end
