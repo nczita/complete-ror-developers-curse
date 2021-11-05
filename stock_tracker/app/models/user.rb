d58cf31893516fc7a099c793e028c0b59be4080c
Luke Dziewanowski <lukasz.dziewanowski@gmail.com>
2021-11-05 15:54:34 +0100

    Add friends and friendships

--- stock_tracker/app/models/user.rb
+++ stock_tracker/app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   validates_length_of :stocks, maximum: MAX_OF_STOCKS
 
