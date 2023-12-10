fe292f4f0b5e78bc19fbe2a6e10fa242d4d456a1
Łukasz Dziewanowski <lukasz.dziewanowski@gmail.com>
2023-12-10 20:42:16 +0100

    JS response for search

--- stock_tracker/app/controllers/users_controller.rb
+++ stock_tracker/app/controllers/users_controller.rb
@@ -8,6 +8,24 @@ class UsersController < ApplicationController
   end
 
   def search
-    render json: params[:friend]
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        flash.now[:alert] = 'Please provide a friend to search'
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else
+      flash_alert_and_back_to_my_portfolio 'Please provide a symbol to search'
+      flash.now[:alert] = 'Please provide a friend to search'
+      respond_to do |format|
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
   end
 end
